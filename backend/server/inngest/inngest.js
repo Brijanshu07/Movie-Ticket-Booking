62135d530253e9a915f46da8b590e502787a5fc4
Brijanshu Chauhan <brijanshuchauhan7011@gmail.com>
2025-06-24 11:43:39 +0530

    initialised backend

---
+++ backend/server/inngest/inngest.js
@@ -0,0 +1,62 @@
+import { Inngest } from "inngest";
+import User from "../models/User.js";
+
+export const inngest = new Inngest({ id: "ticket-movie-ki-hi" });
+
+//function to save user data
+const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+  },
+  {
+    event: "clerk/user.created",
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+  }
+);
+
+//function to delete user
+const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-with-clerk",
+  },
+  {
+    event: "clerk/user.deleted",
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await User.findByIdAndDelete(id);
+  }
+);
+
+//function to update user
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+  },
+  {
+    event: "clerk/user.updated",
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
---
+++ backend/server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
+import connectDB from "./configs/db.js";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./inngest/inngest.js";
+const app = express();
+
+const port = 3000;
+await connectDB();
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Server is live");
+});
+
+app.use("/api/ingest", serve({ client: inngest, functions }));
+
+app.listen(port, () => console.log(`Server is running at ${port} `));
