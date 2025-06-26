d931189340953433b9040428510d67af5d437aef
Brijanshu Chauhan <brijanshuchauhan7011@gmail.com>
2025-06-26 16:14:52 +0530

    email

--- backend/nodeMailer.js
+++ backend/nodeMailer.js
@@ -1,44 +1,61 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 import nodemailer from "nodemailer";
 
+// const user = process.env.SMTP_USER;
+// const pass = process.env.SMTP_PASS;
+
+// console.log("SMTP creds:", user, pass);
+
+// const sendEmail = async ({ to, subject, body }) => {
+//   console.log("SMTP creds:", user, pass); // 👈 Should now print correctly
+
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp-relay.brevo.com",
+//     port: 587,
+//     auth: {
+//       user,
+//       pass,
+//     },
+//   });
+//   console.log("transporter", transporter);
+//   try {
+//     const response = await transporter.sendMail({
+//       from: process.env.SENDER_EMAIL,
+//       to,
+//       subject,
+//       html: body,
+//     });
+
+//     console.log("Email sent successfully:", response.messageId || response);
+//     return response;
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     throw error;
+//   }
+// };
+
+// export default sendEmail;
+const user = process.env.SMTP_USER;
+const pass = process.env.SMTP_PASS;
+// console.log("SMTP creds:", user, pass);
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user,
+    pass,
   },
 });
-
-// console.log("hello");
-
-// const sendEmail = async ({ to, subject, body }) => {
-//   // console.log("Sending email to:", to);
-//   const response = await transporter.sendMail({
-//     from: process.env.SENDER_EMAIL,
-//     to,
-//     subject,
-//     html: body,
-//   });
-//   return response;
-// };
-
-const sendEmail = async ({ to, subject, body }) => {
-  // console.log("Sending email to:", to); // 👈 Ensure function is called
-
-  try {
-    const response = await transporter.sendMail({
-      from: process.env.SENDER_EMAIL,
-      to,
-      subject,
-      html: body,
-    });
-
-    // console.log("Email sent successfully:", response.messageId || response);
-    return response;
-  } catch (error) {
-    // console.error("Error sending email:", error); // 👈 Catch all errors
-    throw error; // Optional: Let it bubble up if needed
-  }
+// console.log("Smtp cre:", process.env.SMTP_USER);
+const sendEmail = async ({ to, from, subject, body }) => {
+  const response = await transporter.sendMail({
+    from: process.env.SENDER_EMAIL,
+    to,
+    subject,
+    html: body,
+  });
+  return response;
 };
+
 export default sendEmail;
--- backend/test/test.js
+++
@@ -1,7 +0,0 @@
-import sendEmail from "../nodeMailer.js";
-
-sendEmail({
-  to: "brijanshuchauhan7011@gmail.com",
-  subject: "Manual Email Test",
-  body: "<p>This is a manual test.</p>",
-});
