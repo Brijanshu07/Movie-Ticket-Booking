4f549f905a11c66abfaf8b5ee745d39be373c5bb
Brijanshu Chauhan <brijanshuchauhan7011@gmail.com>
2025-06-26 17:22:13 +0530

    Added readme

--- frontend/src/lib/timeFormat.js
+++
@@ -1,7 +0,0 @@
-const timeFormat = (minutes) => {
-  const hours = Math.floor(minutes / 60);
-  const minutesLeft = minutes % 60;
-  return `${hours}h ${minutesLeft}m`;
-};
-
-export default timeFormat;
